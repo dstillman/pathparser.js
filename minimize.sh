c21c836fca5999306c9f16e8c99e01510f211aff
Dan Stillman <dan@danstillman.com>
2016-01-13 02:26:44 -0500

    Update minimized version

--- minimize.sh
+++ minimize.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-uglifyjs -c -m --preamble "/* Copyright (c) 2014 Dan Stillman - MIT License - https://github.com/dstillman/pathparser.js */" pathparser.js > pathparser.min.js
+uglifyjs -c -m --preamble "/* Copyright (c) 2014-2016 Dan Stillman - MIT License - https://github.com/dstillman/pathparser.js */" pathparser.js > pathparser.min.js
